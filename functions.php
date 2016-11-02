e496a402f51473d0472c4419709c5a6483c62844
Taylor Fuqua <tfuqua@gwu.edu>
2016-11-02 15:13:56 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+function goodmeadow_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is for general Ukrops theme styling
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
+  	wp_enqueue_style( 'ukrops-style', get_stylesheet_directory_uri() . '/style.min.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'goodmeadow_enqueue_styles' );
+
+function my_remove_page_template( $pages_templates ) {
+
+    unset( $pages_templates['templates/page-products.php'] );
+    unset( $pages_templates['templates/page-product.php'] );
+
+    return $pages_templates;
+}
+add_filter( 'theme_page_templates', 'my_remove_page_template' );
+
+?>
